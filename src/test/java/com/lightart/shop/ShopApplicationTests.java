e801be8b1a86f62c72f46799600737708d965e66
daniel1997499 <daniel1997499@gmail.com>
2021-08-15 13:58:11 +0300

    Initial commit

---
+++ src/test/java/com/lightart/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lightart.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
